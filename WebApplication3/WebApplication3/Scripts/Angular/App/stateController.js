641313e16866cd7f4e9c7be8c95e35b1b79afed1
myurnaikwadi <mayunaikwadi@gmail.com>
2016-05-07 15:14:48 +0530

    Test 1

--- WebApplication3/WebApplication3/Scripts/Angular/App/stateController.js
+++ WebApplication3/WebApplication3/Scripts/Angular/App/stateController.js
@@ -1,4 +1,3 @@
-/// <reference path="D:\Visual Studio 2015\Projects\Arati\WebApplication\MVC\WebApplication3\WebApplication3\Views/Login/login.html" />
 app.config(['$stateProvider', '$urlRouterProvider','$locationProvider',function($stateProvider, $urlRouterProvider,$locationProvider){
 	$urlRouterProvider.otherwise('login');
 	$stateProvider	
