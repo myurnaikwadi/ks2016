731f11e9d453e88c191953ea9d26ffee7d36ff53
myurnaikwadi <mayunaikwadi@gmail.com>
2016-05-07 15:52:06 +0530

    Remove error

--- WebApplication3/WebApplication3/Scripts/Angular/Services/ksService.js
+++ WebApplication3/WebApplication3/Scripts/Angular/Services/ksService.js
@@ -1,31 +1,31 @@
 ﻿app.factory('authentification', function ($http) {
- +    return {
- +        //$http.get('/someUrl', config).then(successCallback, errorCallback);
- +        //$http.post('/someUrl', data, config).then(successCallback, errorCallback);
- +        /**
- +         * @auther : MKN
- +         * @date : 07/05/2016
- +         * @Purpose : login function - send credienal to verify details
- +         */
- +        login: function (iObj) {
- +            $http.get('/plateformLogin', iObj.signupObject).then(iObj.successCallBack, iObj.failureCallBack);
- +        },
- +        /**
- +         * @auther : MKN
- +         * @date : 07/05/2016
- +         * @Purpose : logout function - reset all data 
- +         */
- +        logout: function (iObj) {
- +            $http.get('/plateformLogout', iObj.signupObject).then(iObj.successCallBack, iObj.failureCallBack);
- +        },
- +        /**
- +         * @auther : MKN
- +         * @date : 07/05/2016
- +         * @Purpose : Signup click function - send credienal to verify details
- +         */
- +        signup: function (iObj) {
- +            console.error(iObj)
- +            $http.get('/plateformSignup', iObj.signupObject).then(iObj.successCallBack,iObj.failureCallBack);
- +        }
- +    }
- +});
+     return {
+         //$http.get('/someUrl', config).then(successCallback, errorCallback);
+         //$http.post('/someUrl', data, config).then(successCallback, errorCallback);
+        /**
+          * @auther : MKN
+          * @date : 07/05/2016
+          * @Purpose : login function - send credienal to verify details
+          */
+         login: function (iObj) {
+             $http.get('/plateformLogin', iObj.signupObject).then(iObj.successCallBack, iObj.failureCallBack);
+         },
+         /**
+          * @auther : MKN
+          * @date : 07/05/2016
+          * @Purpose : logout function - reset all data 
+          */
+         logout: function (iObj) {
+             $http.get('/plateformLogout', iObj.signupObject).then(iObj.successCallBack, iObj.failureCallBack);
+         },
+         /**
+          * @auther : MKN
+          * @date : 07/05/2016
+          * @Purpose : Signup click function - send credienal to verify details
+          */
+         signup: function (iObj) {
+             console.error(iObj)
+             $http.get('/plateformSignup', iObj.signupObject).then(iObj.successCallBack,iObj.failureCallBack);
+         }
+     }
+ });
